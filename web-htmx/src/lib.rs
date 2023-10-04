4e279ea7c2992d0a2a7a01604d2144bc682b764f
Ian <bontaq@gmail.com>
2023-10-04 17:38:04 -0400

    Add render + a component

--- web-htmx/src/lib.rs
+++ web-htmx/src/lib.rs
@@ -1,18 +1,29 @@
+#![feature(proc_macro_hygiene)]
+
 use axum::{
     response::Html,
     Router, 
     routing::get,
 };
+use render::{component, rsx, html};
 
 pub fn routes() -> Router {
     Router::new()
         .route("/", get(get_home))    
 }
 
+
+#[component]
+fn Heading<'title>(title: &'title str) {
+  rsx! { <h1 class={"title"}>{title}</h1> }
+}
+
 async fn get_home() -> Html<String> {
-    Html(
-        "<marquee>ONE SMALL STEP FOR AN HTML_ASSHOLE, ONE GIANT LEAP FOR HTML_ASSHOLEKIND</marquee>".into()
-    )
+    html! {
+        <marquee>
+            <Heading title={"hello!"}></Heading>
+        </marquee>
+    }.into()
 }
 
 #[cfg(test)]
