cecb67bf760c853381d65593deb5e4bc5062fe15
Paul Bouzakis <888851+pbouzakis@users.noreply.github.com>
2023-10-04 16:50:38 -0400

    Setup our server w our first route.

--- main/src/main.rs
+++ main/src/main.rs
@@ -1,5 +1,25 @@
-use web_htmx::add;
+use std::net::SocketAddr;
+use axum::{
+    response::IntoResponse,
+    Router, 
+    routing::get,
+};
 
-fn main() {
-    println!("Hello, world! {}", add(2, 5));
+#[tokio::main]
+async fn main() {
+    let app = Router::new()
+    .route("/healthcheck", get(get_health_check));
+
+    // run our app with hyper
+    // `axum::Server` is a re-export of `hyper::Server`
+    let addr = SocketAddr::from(([0, 0, 0, 0], 3000));
+
+    axum::Server::bind(&addr)
+        .serve(app.into_make_service())
+        .await
+        .expect("Failed to start server");
+}
+
+async fn get_health_check() -> impl IntoResponse {
+    "ONE SMALL STEP FOR AN ASSHOLE, ONE GIANT LEAP FOR ASSHOLEKIND"
 }
