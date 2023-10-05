e7d69df8016ff3bceb700393f5ffacc3c96896dc
Paul Bouzakis <888851+pbouzakis@users.noreply.github.com>
2023-10-05 15:37:53 -0400

    Add htmx w/ a test route.

--- web-client/src/lib.rs
+++ web-client/src/lib.rs
@@ -1,8 +1,4 @@
-use axum::{
-    // response::Html,
-    Router, 
-    // routing::get,
-};
+use axum::Router;
 use tower_http::services::ServeDir;
 use rscx::{component, html, props};
 
@@ -13,6 +9,9 @@ pub fn routes() -> Router {
 
 #[props]
 pub struct HtmlLayoutProps {
+    #[builder(setter(into), default = "Yall Chart".to_string())]
+    head_title: String,
+
     #[builder(default)]
     head_links: String,
 
@@ -27,13 +26,16 @@ pub struct HtmlLayoutProps {
 pub fn HtmlLayout(props: HtmlLayoutProps) -> String {
     html! {
         <!DOCTYPE html>
-        <html>
+        <html lang="en">
             <head>
+                <meta charset="utf-8" />
+                <meta name="viewport" content="width=device-width, initial-scale=1" />
+                <title>{props.head_title}</title>
                 {props.head_links}
                 {props.head_scripts}
             </head>
             <body>
-                { props.children }
+                {props.children}
                 <script src="/client/common.js"></script>
             </body>
         </html>
--- web-htmx/src/lib.rs
+++ web-htmx/src/lib.rs
@@ -12,6 +12,7 @@ use web_client::{
 pub fn routes() -> Router {
     Router::new()
         .route("/", get(get_home))
+        .route("/htmx", get(htmx_test))
         .nest_service("/client", client_routes())   
 }
 
@@ -38,16 +39,31 @@ fn Welcome(props: WelcomeProps) -> String {
 
 async fn get_home() -> Html<String> {
     Html(html! {
-        <HtmlLayout>
+        <HtmlLayout
+            head_scripts={
+                html! {
+                    <script 
+                        src="https://unpkg.com/htmx.org@1.9.5" 
+                        integrity="sha384-xcuj3WpfgjlKF+FXhSQFQ0ZNr39ln+hwjN3npfM9VBnUskLolQAcN80McRIVOPuO" 
+                        crossorigin="anonymous"
+                    ></script>
+                }
+            }
+        >
             <Welcome 
                 title="Yall Ready for This?"
             >
                 <marquee>
                     "I didn't think so!"
                 </marquee>
+                <button hx-get="/htmx" hx-swap="outerHTML">Click me!</button>
             </Welcome>
         </HtmlLayout>
     })
+}
+
+async fn htmx_test() -> Html<String> {
+    Html("Is this the real life? Is this just fantasy?".into())
 }
 
 #[cfg(test)]
