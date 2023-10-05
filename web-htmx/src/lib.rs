15baf13d3660afdbea79ea48f67a0067ebbf766f
Paul Bouzakis <888851+pbouzakis@users.noreply.github.com>
2023-10-05 15:10:11 -0400

    Update web-htmx to use client ccrate.

--- web-htmx/src/lib.rs
+++ web-htmx/src/lib.rs
@@ -4,12 +4,16 @@ use axum::{
     routing::get,
 };
 use rscx::{component, html, props};
+use web_client::{
+    routes as client_routes,
+    HtmlLayout,
+};
 
 pub fn routes() -> Router {
     Router::new()
-        .route("/", get(get_home))    
+        .route("/", get(get_home))
+        .nest_service("/client", client_routes())   
 }
-
 
 #[props]
 /// mark a struct with #[props] to use it as props in a component.
@@ -24,33 +28,30 @@ struct WelcomeProps {
 
 #[component]
 fn Welcome(props: WelcomeProps) -> String {
-  html! { 
-    <main>
-        <h1>{ props.title }</h1>
-        { props.children }
-    </main>
- }
+    html! { 
+        <main>
+            <h1>{ props.title }</h1>
+            { props.children }
+        </main>
+    }
 }
 
 async fn get_home() -> Html<String> {
     Html(html! {
-        <!DOCTYPE html>
-        <html>
-            <body>
-                <Welcome title="Yall Ready for This?">
-                    <marquee>
-                        "I didn't think so!"
-                    </marquee>
-                </Welcome>
-            </body>
-        </html>
+        <HtmlLayout>
+            <Welcome 
+                title="Yall Ready for This?"
+            >
+                <marquee>
+                    "I didn't think so!"
+                </marquee>
+            </Welcome>
+        </HtmlLayout>
     })
 }
 
 #[cfg(test)]
 mod tests {
-    use super::*;
-
     #[test]
     fn it_works() {
         assert_eq!(4, 4);
