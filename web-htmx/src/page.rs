230d43b9acf3f560898b76a2e2ddbce81d978ad1
Paul Bouzakis <888851+pbouzakis@users.noreply.github.com>
2023-10-07 19:22:25 -0400

    Add WebLayout component w/ basic structure.

---
+++ web-htmx/src/page.rs
@@ -0,0 +1,35 @@
+use rscx::{component, html, props};
+use web_client::HtmlLayout;
+
+#[props]
+pub struct PageLayoutProps {
+    #[builder(default)]
+    children: String,
+}
+
+#[component]
+pub fn PageLayout(props: PageLayoutProps) -> String {
+    html! {
+        <HtmlLayout
+            head_scripts={
+                html! {
+                    <script 
+                        src="https://unpkg.com/htmx.org@1.9.5" 
+                        integrity="sha384-xcuj3WpfgjlKF+FXhSQFQ0ZNr39ln+hwjN3npfM9VBnUskLolQAcN80McRIVOPuO" 
+                        crossorigin="anonymous"
+                    ></script>
+                }
+        }
+        >
+            <header class="bg-slate-200 border-y border-b-indigo-500">
+                <h1>Yall Chart</h1>
+            </header>
+            <main>
+                {props.children}
+            </main>
+            <footer class="text-xs">
+                <small>{"2023 &copy; Yall Chart"}</small>
+            </footer>
+        </HtmlLayout>
+    }
+}
