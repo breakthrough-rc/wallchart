08c988c468ccc52964be6709438981ac192f809f
tavish <tapegram@gmail.com>
2023-10-15 15:57:44 -0500

    Add props and types (that are ignored for now)

--- web-htmx/src/components/wallchart.rs
+++ web-htmx/src/components/wallchart.rs
@@ -1,7 +1,46 @@
 use rscx::{component, html, props};
 
+pub struct Worksite {
+    pub id: String,
+    pub name: String,
+    pub locations: Vec<Location>,
+}
+
+pub struct Location {
+    pub id: String,
+    pub name: String,
+    pub shifts: Vec<Shift>,
+}
+
+pub struct Shift {
+    pub id: String,
+    pub name: String,
+    pub workers: Vec<Worker>,
+}
+
+pub struct Worker {
+    pub id: String,
+    pub name: String,
+    pub last_assessment: Assessment,
+    pub tags: Vec<Tag>,
+}
+
+pub struct Assessment {
+    pub id: String,
+    pub value: u8,
+}
+
+pub struct Tag {
+    pub id: String,
+    pub name: String,
+    pub icon: String,
+}
+
 #[props]
-pub struct WallchartProps {}
+pub struct WallchartProps {
+    #[builder(setter(into))]
+    worksite: Worksite,
+}
 
 #[component]
 pub fn Wallchart(props: WallchartProps) -> String {
--- web-htmx/src/pages/wallchart.rs
+++ web-htmx/src/pages/wallchart.rs
@@ -1,12 +1,22 @@
 use axum::response::Html;
 use rscx::html;
 
-use crate::{components::wallchart::Wallchart, page::PageLayout};
+use crate::{
+    components::wallchart::{Wallchart, Worksite},
+    page::PageLayout,
+};
 
 pub async fn get_wallchart_page() -> Html<String> {
+    let worksite = Worksite {
+        id: "1".to_string(),
+        name: "Dunder Mifflin".to_string(),
+        locations: vec![],
+    };
     Html(html! {
     <PageLayout>
-        <Wallchart />
+        <div class="my-4">
+            <Wallchart worksite=worksite/>
+        </div>
     </PageLayout>
     })
 }
