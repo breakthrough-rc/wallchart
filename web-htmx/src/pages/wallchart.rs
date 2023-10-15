7db921c5b648596892c318f58364e285a8746226
tavish <tapegram@gmail.com>
2023-10-15 14:35:06 -0500

    Scaffold a rough harcoded draft of the wallchart page

--- web-htmx/src/pages/wallchart.rs
+++ web-htmx/src/pages/wallchart.rs
@@ -9,11 +9,7 @@ pub async fn get_wallchart_page() -> Html<String> {
       <div class="px-4 sm:px-6 lg:px-8">
         <div class="sm:flex sm:items-center">
           <div class="sm:flex-auto">
-            <h1 class="text-base font-semibold leading-6 text-gray-900">Users</h1>
-            <p class="mt-2 text-sm text-gray-700">A list of all the users in your account including their name, title, email and role.</p>
-          </div>
-          <div class="mt-4 sm:ml-16 sm:mt-0 sm:flex-none">
-            <button type="button" class="block rounded-md bg-indigo-600 px-3 py-2 text-center text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600">Add user</button>
+            <h1 class="text-base font-semibold leading-6 text-gray-900">Dunder Mifflin</h1>
           </div>
         </div>
         <div class="mt-8 flow-root">
@@ -23,38 +19,88 @@ pub async fn get_wallchart_page() -> Html<String> {
                 <thead class="bg-white">
                   <tr>
                     <th scope="col" class="py-3.5 pl-4 pr-3 text-left text-sm font-semibold text-gray-900 sm:pl-3">Name</th>
-                    <th scope="col" class="px-3 py-3.5 text-left text-sm font-semibold text-gray-900">Title</th>
-                    <th scope="col" class="px-3 py-3.5 text-left text-sm font-semibold text-gray-900">Email</th>
-                    <th scope="col" class="px-3 py-3.5 text-left text-sm font-semibold text-gray-900">Role</th>
-                    <th scope="col" class="relative py-3.5 pl-3 pr-4 sm:pr-3">
-                      <span class="sr-only">Edit</span>
-                    </th>
+                    <th scope="col" class="px-3 py-3.5 text-left text-sm font-semibold text-gray-900">Last Assessment</th>
+                    <th scope="col" class="px-3 py-3.5 text-left text-sm font-semibold text-gray-900">Tags</th>
                   </tr>
                 </thead>
                 <tbody class="bg-white">
                   <tr class="border-t border-gray-200">
-                    <th colspan="5" scope="colgroup" class="bg-gray-50 py-2 pl-4 pr-3 text-left text-sm font-semibold text-gray-900 sm:pl-3">Edinburgh</th>
+                    <th colspan="3" scope="colgroup" class="bg-gray-50 py-2 pl-4 pr-3 text-left text-sm font-semibold text-gray-900 sm:pl-3">
+                      Office - Day
+                    </th>
                   </tr>
                   <tr class="border-t border-gray-300">
-                    <td class="whitespace-nowrap py-4 pl-4 pr-3 text-sm font-medium text-gray-900 sm:pl-3">Lindsay Walton</td>
-                    <td class="whitespace-nowrap px-3 py-4 text-sm text-gray-500">Front-end Developer</td>
-                    <td class="whitespace-nowrap px-3 py-4 text-sm text-gray-500">lindsay.walton@example.com</td>
-                    <td class="whitespace-nowrap px-3 py-4 text-sm text-gray-500">Member</td>
-                    <td class="relative whitespace-nowrap py-4 pl-3 pr-4 text-right text-sm font-medium sm:pr-3">
-                      <a href="#" class="text-indigo-600 hover:text-indigo-900">Edit<span class="sr-only">, Lindsay Walton</span></a>
-                    </td>
+                    <td class="whitespace-nowrap py-4 pl-4 pr-3 text-sm font-medium text-gray-900 sm:pl-3">Jim Halpert</td>
+                    <td class="whitespace-nowrap px-3 py-4 text-sm text-gray-500">1</td>
+                    <td class="whitespace-nowrap px-3 py-4 text-sm text-gray-500">"🔴 🔵 🟢 🟡"</td>
                   </tr>
-                  <tr class="border-t border-gray-200">
-                    <td class="whitespace-nowrap py-4 pl-4 pr-3 text-sm font-medium text-gray-900 sm:pl-3">Courtney Henry</td>
-                    <td class="whitespace-nowrap px-3 py-4 text-sm text-gray-500">Designer</td>
-                    <td class="whitespace-nowrap px-3 py-4 text-sm text-gray-500">courtney.henry@example.com</td>
-                    <td class="whitespace-nowrap px-3 py-4 text-sm text-gray-500">Admin</td>
-                    <td class="relative whitespace-nowrap py-4 pl-3 pr-4 text-right text-sm font-medium sm:pr-3">
-                      <a href="#" class="text-indigo-600 hover:text-indigo-900">Edit<span class="sr-only">, Courtney Henry</span></a>
-                    </td>
+                  <tr class="border-t border-gray-300">
+                    <td class="whitespace-nowrap py-4 pl-4 pr-3 text-sm font-medium text-gray-900 sm:pl-3">Pam Beesly</td>
+                    <td class="whitespace-nowrap px-3 py-4 text-sm text-gray-500">2</td>
+                    <td class="whitespace-nowrap px-3 py-4 text-sm text-gray-500">"🟢 🟡"</td>
+                  </tr>
+                  <tr class="border-t border-gray-300">
+                    <td class="whitespace-nowrap py-4 pl-4 pr-3 text-sm font-medium text-gray-900 sm:pl-3">Dwight Schrute</td>
+                    <td class="whitespace-nowrap px-3 py-4 text-sm text-gray-500">4</td>
+                    <td class="whitespace-nowrap px-3 py-4 text-sm text-gray-500">""</td>
+                  </tr>
+                  <tr class="border-t border-gray-300">
+                    <td class="whitespace-nowrap py-4 pl-4 pr-3 text-sm font-medium text-gray-900 sm:pl-3">Creed Bratton</td>
+                    <td class="whitespace-nowrap px-3 py-4 text-sm text-gray-500">3</td>
+                    <td class="whitespace-nowrap px-3 py-4 text-sm text-gray-500">"🟢 🟡"</td>
+                  </tr>
+                  <tr class="border-t border-gray-300">
+                    <td class="whitespace-nowrap py-4 pl-4 pr-3 text-sm font-medium text-gray-900 sm:pl-3">Angela Martin</td>
+                    <td class="whitespace-nowrap px-3 py-4 text-sm text-gray-500">5</td>
+                    <td class="whitespace-nowrap px-3 py-4 text-sm text-gray-500">"🟢"</td>
                   </tr>
 
-                  <!-- "More people..." -->
+                  <tr class="border-t border-gray-200">
+                    <th colspan="3" scope="colgroup" class="bg-gray-50 py-2 pl-4 pr-3 text-left text-sm font-semibold text-gray-900 sm:pl-3">
+                      Warehouse - Day
+                    </th>
+                  </tr>
+                  <tr class="border-t border-gray-300">
+                    <td class="whitespace-nowrap py-4 pl-4 pr-3 text-sm font-medium text-gray-900 sm:pl-3">Darryl Philbin</td>
+                    <td class="whitespace-nowrap px-3 py-4 text-sm text-gray-500">1</td>
+                    <td class="whitespace-nowrap px-3 py-4 text-sm text-gray-500">"🔵 🟢 🟡"</td>
+                  </tr>
+                  <tr class="border-t border-gray-300">
+                    <td class="whitespace-nowrap py-4 pl-4 pr-3 text-sm font-medium text-gray-900 sm:pl-3">Nate Nickerson</td>
+                    <td class="whitespace-nowrap px-3 py-4 text-sm text-gray-500">3</td>
+                    <td class="whitespace-nowrap px-3 py-4 text-sm text-gray-500">"🟡"</td>
+                  </tr>
+                  <tr class="border-t border-gray-300">
+                    <td class="whitespace-nowrap py-4 pl-4 pr-3 text-sm font-medium text-gray-900 sm:pl-3">Roy Anderson</td>
+                    <td class="whitespace-nowrap px-3 py-4 text-sm text-gray-500">3</td>
+                    <td class="whitespace-nowrap px-3 py-4 text-sm text-gray-500">""</td>
+                  </tr>
+                  <tr class="border-t border-gray-300">
+                    <td class="whitespace-nowrap py-4 pl-4 pr-3 text-sm font-medium text-gray-900 sm:pl-3">Madge Madsen</td>
+                    <td class="whitespace-nowrap px-3 py-4 text-sm text-gray-500">4</td>
+                    <td class="whitespace-nowrap px-3 py-4 text-sm text-gray-500">""</td>
+                  </tr>
+
+                  <tr class="border-t border-gray-200">
+                    <th colspan="3" scope="colgroup" class="bg-gray-50 py-2 pl-4 pr-3 text-left text-sm font-semibold text-gray-900 sm:pl-3">
+                      Warehouse - Night
+                    </th>
+                  </tr>
+                  <tr class="border-t border-gray-300">
+                    <td class="whitespace-nowrap py-4 pl-4 pr-3 text-sm font-medium text-gray-900 sm:pl-3">Val Johnson</td>
+                    <td class="whitespace-nowrap px-3 py-4 text-sm text-gray-500">2</td>
+                    <td class="whitespace-nowrap px-3 py-4 text-sm text-gray-500">"🔵 🟢 🟡"</td>
+                  </tr>
+                  <tr class="border-t border-gray-300">
+                    <td class="whitespace-nowrap py-4 pl-4 pr-3 text-sm font-medium text-gray-900 sm:pl-3">Lonny Collins</td>
+                    <td class="whitespace-nowrap px-3 py-4 text-sm text-gray-500">1</td>
+                    <td class="whitespace-nowrap px-3 py-4 text-sm text-gray-500">"🟡"</td>
+                  </tr>
+                  <tr class="border-t border-gray-300">
+                    <td class="whitespace-nowrap py-4 pl-4 pr-3 text-sm font-medium text-gray-900 sm:pl-3">Frank</td>
+                    <td class="whitespace-nowrap px-3 py-4 text-sm text-gray-500">3</td>
+                    <td class="whitespace-nowrap px-3 py-4 text-sm text-gray-500">""</td>
+                  </tr>
                 </tbody>
               </table>
             </div>
