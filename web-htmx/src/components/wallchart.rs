a95979b016fbcaeb7de0df687c4d4290ee55a2f9
Paul Bouzakis <888851+pbouzakis@users.noreply.github.com>
2023-10-18 16:52:03 -0400

    Add resources handler for delete worker from shift.

--- web-htmx/src/components/wallchart.rs
+++ web-htmx/src/components/wallchart.rs
@@ -100,13 +100,21 @@ pub struct WorkerRowProps {
 #[component]
 pub fn WorkerRow(props: WorkerRowProps) -> String {
     html! {
-       <tr class="border-t border-gray-300">
-         <td class="whitespace-nowrap py-4 pl-4 pr-3 text-sm font-medium text-gray-900 sm:pl-3">{props.worker.name}</td>
-         <td class="whitespace-nowrap px-3 py-4 text-sm text-gray-500">{props.worker.last_assessment.value}</td>
-         <td class="whitespace-nowrap px-3 py-4 text-sm text-gray-500">{props.worker.tags.into_iter().map(|tag| tag.icon).collect_fragment()}</td>
-         <td class="whitespace-nowrap px-3 py-4 text-sm text-gray-500">
-           <button type="button" hx-delete={format!("{}/workers/{}", props.shift_path, props.worker.id)} class="rounded bg-white px-2 py-1 text-xs font-semibold text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 hover:bg-gray-50">Remove</button>
-         </td>
-       </tr>
+      <tr class="border-t border-gray-300">
+          <td class="whitespace-nowrap py-4 pl-4 pr-3 text-sm font-medium text-gray-900 sm:pl-3">{props.worker.name}</td>
+          <td class="whitespace-nowrap px-3 py-4 text-sm text-gray-500">{props.worker.last_assessment.value}</td>
+          <td class="whitespace-nowrap px-3 py-4 text-sm text-gray-500">{props.worker.tags.into_iter().map(|tag| tag.icon).collect_fragment()}</td>
+          <td class="whitespace-nowrap px-3 py-4 text-sm text-gray-500">
+              <button 
+                type="button" 
+                hx-delete={format!("{}/workers/{}", props.shift_path, props.worker.id)} 
+                class="rounded bg-white px-2 py-1 text-xs font-semibold text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 hover:bg-gray-50"
+                hx-swap="outerHTML swap:1s" 
+                hx-target="closest tr"
+              >
+                  Remove
+                </button>
+          </td>
+      </tr>
     }
 }
--- web-htmx/src/lib.rs
+++ web-htmx/src/lib.rs
@@ -1,6 +1,7 @@
-use axum::{http::HeaderMap, response::Html, routing::get, Router};
+use axum::{http::HeaderMap, response::Html, routing::{get, delete}, Router};
 use page::PageLayout;
 use pages::{wallchart::get_wallchart_page, workers::get_workers_new_page};
+use resources::worksite::delete_worker_from_shift;
 use rscx::{component, html, props};
 use state::WebHtmxState;
 use web_client::routes as client_routes;
@@ -16,6 +17,7 @@ pub fn routes(state: WebHtmxState) -> Router {
     Router::new()
         .route("/", get(get_home))
         .route("/wallchart", get(get_wallchart_page))
+        .route("/worksites/:worksite_id/locations/:location_id/shifts/:shift_id/workers/:worker_id", delete(delete_worker_from_shift))
         .route("/workers/new", get(get_workers_new_page))
         .route("/test-render", get(get_test_render))
         .route("/htmx", get(htmx_test))
