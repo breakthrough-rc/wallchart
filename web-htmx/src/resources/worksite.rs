9f4bddea82bc49fe4d03cb3eab49b8aef92d08d0
Paul Bouzakis <888851+pbouzakis@users.noreply.github.com>
2023-10-19 16:25:45 -0400

    Implement removing of worker from shift.

--- web-htmx/src/resources/worksite.rs
+++ web-htmx/src/resources/worksite.rs
@@ -1,19 +1,24 @@
-use axum::extract;
+use crate::state::WebHtmxState;
+use axum::extract::{self, State};
 
 pub async fn delete_worker_from_shift(
-  extract::Path((
-    worksite_id,
-    location_id,
-    shift_id,
-    worker_id,
-)): extract::Path<(String, String, String, String)>) -> String {
+    extract::Path((worksite_id, location_id, shift_id, worker_id)): extract::Path<(
+        String,
+        String,
+        String,
+        String,
+    )>,
+    State(WebHtmxState { worksite_service }): State<WebHtmxState>,
+) -> String {
     println!(
-        "Delete worker: {} from shift: {}, from worksite: {} in location: {}", 
-        worker_id, 
-        shift_id,
-        worksite_id,
-        location_id,
+        "Delete worker: {} from shift: {}, from worksite: {} in location: {}",
+        worker_id, shift_id, worksite_id, location_id,
     );
+
+    worksite_service
+        .remove_worker_from_shift(worksite_id, shift_id, worker_id)
+        .await
+        .unwrap();
 
     "".to_string()
 }
