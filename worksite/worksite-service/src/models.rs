d26e88678b694276fce77699114b9e65bed92979
tavish <tapegram@gmail.com>
2023-10-26 15:24:29 -0500

    scaffold some initial events

--- worksite/worksite-service/src/models.rs
+++ worksite/worksite-service/src/models.rs
@@ -39,3 +39,42 @@ pub struct Tag {
     pub name: String,
     pub icon: String,
 }
+
+pub enum Events {
+    WorksiteCreated(WorksiteCreatedEvent),
+    LocationAdded(LocationAddedEvent),
+    ShiftAdded(ShiftAddedEvent),
+    WorkerCreated(WorkerCreatedEvent),
+    ShiftAssigned(ShiftAssignedEvent),
+    ShiftUnassigned(ShiftUnassignedEvent),
+}
+
+pub struct WorksiteCreatedEvent {
+    pub id: String,
+    pub name: String,
+}
+
+pub struct LocationAddedEvent {
+    pub id: String,
+    pub name: String,
+}
+
+pub struct ShiftAddedEvent {
+    pub id: String,
+    pub name: String,
+}
+
+pub struct WorkerCreatedEvent {
+    pub id: String,
+    pub name: String,
+}
+
+pub struct ShiftAssignedEvent {
+    pub shift_id: String,
+    pub worker_id: String,
+}
+
+pub struct ShiftUnassignedEvent {
+    pub shift_id: String,
+    pub worker_id: String,
+}
