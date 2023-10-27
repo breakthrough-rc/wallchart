acf2fd12cd5806ea05eb3f873be31fd864d4c0be
Paul Bouzakis <888851+pbouzakis@users.noreply.github.com>
2023-10-26 20:47:54 -0400

    Support profiledropdown toggle with new control.

--- web-htmx/src/components/appshell.rs
+++ web-htmx/src/components/appshell.rs
@@ -209,9 +209,9 @@ struct ProfileDropdownProps {
 #[component]
 fn ProfileDropdown(props: ProfileDropdownProps) -> String {
     html! {
-        <div class="relative ml-3">
+        <div class="relative ml-3" data-yc-control="toggle">
             <div>
-                <button type="button" class="relative flex max-w-xs items-center rounded-full bg-white text-sm focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2" id="user-menu-button" aria-expanded="false" aria-haspopup="true">
+                <button type="button" data-toggle-action class="relative flex max-w-xs items-center rounded-full bg-white text-sm focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2" id="user-menu-button" aria-expanded="false" aria-haspopup="true">
                     <span class="absolute -inset-1.5"></span>
                     <span class="sr-only">Open user menu</span>
                     <img class="h-8 w-8 rounded-full" src="https://images.unsplash.com/photo-1472099645785-5658abf4ff4e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80" alt="" />
@@ -242,6 +242,7 @@ fn ProfileDropdown(props: ProfileDropdownProps) -> String {
                         .collect_fragment()
                 }
             </Transition>
+            <script>"YcControls.attach(document.currentScript.parentElement)"</script>
         </div>
     }
 }
