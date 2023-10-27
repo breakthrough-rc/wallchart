fb1700fedc11fa4807ba704fe8224423f836c381
Paul Bouzakis <888851+pbouzakis@users.noreply.github.com>
2023-10-26 20:46:25 -0400

    Update registry to attach before ready; Add ready hook.

--- web-client/src/client/controls.ts
+++ web-client/src/client/controls.ts
@@ -1,12 +1,16 @@
 import registry from "./controls/registery";
 import events from "./controls/events";
 import Notifications from "./controls/Notification";
+import Toggle from "./controls/Toggle";
 
 function init() {
   registry.init();
 
   events.init(registry);
   Notifications.init(registry);
+  Toggle.init(registry);
+
+  registry.ready();
 }
 
 export default {
