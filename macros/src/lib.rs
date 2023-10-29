2dc4c4fbaf11792b4eeeb443164c44e5c75c9beb
Paul Bouzakis <888851+pbouzakis@users.noreply.github.com>
2023-10-29 19:55:43 -0400

    Add help method for html_attrs props to conver to hashmap.

--- macros/src/lib.rs
+++ macros/src/lib.rs
@@ -1,3 +1,9 @@
+/*
+ To add an attribute to the html_attrs macro,
+   - add a field to the struct
+   - add the attribute to the `to_hashmap` function.
+*/
+
 #[macro_export]
 macro_rules! html_attrs {
     ($pub:vis struct $name:ident { $(#[$x:meta] $fpub:vis $field:ident : $type:ty,)* }) => {
@@ -34,6 +40,22 @@ macro_rules! html_attrs {
                 #[$x]
                 $fpub $field: $type,
             )*
+        }
+
+        impl $name {
+            fn html_attrs_to_hashmap(&self) -> std::collections::HashMap<&'static str, String> {
+                let mut map = std::collections::HashMap::new();
+
+                map.insert("id", self.id.clone());
+                map.insert("class", self.class.clone());
+                map.insert("onclick", self.onclick.clone());
+                map.insert("role", self.role.clone());
+                map.insert("aria-orientation", self.aria_orientation.clone());
+                map.insert("aria-labelledby", self.aria_labelledby.clone());
+                map.insert("tabindex", self.tabindex.clone());
+
+                map
+            }
         }
     };
 }
