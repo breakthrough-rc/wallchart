34e444fd88c1ff06debd208051477df95e068744
Paul Bouzakis <888851+pbouzakis@users.noreply.github.com>
2023-10-28 20:49:26 -0400

    Trim attributes string when creating HtmlElement.

--- web-client/src/server/html_element.rs
+++ web-client/src/server/html_element.rs
@@ -21,20 +21,41 @@ pub struct HtmlElementProps {
 
     #[builder(default=String::from("div"))]
     tag: String,
+
+    #[builder(default=String::from("HtmlElement"))]
+    component_name: String,
 }
 
 #[component]
 pub fn HtmlElement(props: HtmlElementProps) -> String {
     let attrs = opt_attrs(HashMap::from([
+        ("data-rsx", props.component_name),
         ("id", props.id),
         ("class", props.class),
         ("role", props.role),
         ("onclick", props.onclick),
     ]));
-    let attrs = format!("data-rsx=\"HtmlElement\" {}", attrs);
+    let attrs = attrs.trim().to_string();
 
     format!(
         "<{} {}>{}</{}>",
         props.tag, attrs, props.children, props.tag
     )
+}
+
+#[cfg(test)]
+mod tests {
+    use rscx::html;
+
+    // Note this useful idiom: importing names from outer (for mod tests) scope.
+    use super::*;
+
+    #[tokio::test]
+    async fn test_with_no_attrs() {
+        let html = html! {
+            <HtmlElement />
+        };
+
+        assert_eq!(html, String::from("<div data-rsx=\"HtmlElement\"></div>"));
+    }
 }
