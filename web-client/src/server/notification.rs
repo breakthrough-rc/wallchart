6d723a3f121cfee6e0a3f4b85536e4d37e9a17ab
Paul Bouzakis <888851+pbouzakis@users.noreply.github.com>
2023-11-07 16:31:08 -0500

    Minor fix.

--- web-client/src/server/notification.rs
+++ web-client/src/server/notification.rs
@@ -48,23 +48,22 @@ pub fn Notification(props: NotificationProps) -> String {
         >
             <div class="p-4">
                 <div class="flex items-start">
+                    <div class="flex-shrink-0">
+                        <NotificationIcon svg=props.icon_svg />
+                    </div>
 
-                <div class="flex-shrink-0">
-                    <NotificationIcon svg=props.icon_svg />
-                </div>
-
-                <div class="ml-3 w-0 flex-1 pt-0.5">
-                    <p class="text-sm font-medium text-gray-900" data-notification-title>{props.title}</p>
-                    <p class="mt-1 text-sm text-gray-500" data-notification-message>{props.message}</p>
-                </div>
-                <div class="ml-4 flex flex-shrink-0">
-                    <button type="button" data-toggle-action="close" data-notification-close class="inline-flex rounded-md bg-white text-gray-400 hover:text-gray-500 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2">
-                        <span class="sr-only">Close</span>
-                        <svg class="h-5 w-5" viewBox="0 0 20 20" fill="currentColor" aria-hidden="true" data-notification-close>
-                            <path d="M6.28 5.22a.75.75 0 00-1.06 1.06L8.94 10l-3.72 3.72a.75.75 0 101.06 1.06L10 11.06l3.72 3.72a.75.75 0 101.06-1.06L11.06 10l3.72-3.72a.75.75 0 00-1.06-1.06L10 8.94 6.28 5.22z" />
-                        </svg>
-                    </button>
-                </div>
+                    <div class="ml-3 w-0 flex-1 pt-0.5">
+                        <p class="text-sm font-medium text-gray-900" data-notification-title>{props.title}</p>
+                        <p class="mt-1 text-sm text-gray-500" data-notification-message>{props.message}</p>
+                    </div>
+                    <div class="ml-4 flex flex-shrink-0">
+                        <button type="button" data-toggle-action="close" data-notification-close class="inline-flex rounded-md bg-white text-gray-400 hover:text-gray-500 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2">
+                            <span class="sr-only">Close</span>
+                            <svg class="h-5 w-5" viewBox="0 0 20 20" fill="currentColor" aria-hidden="true" data-notification-close>
+                                <path d="M6.28 5.22a.75.75 0 00-1.06 1.06L8.94 10l-3.72 3.72a.75.75 0 101.06 1.06L10 11.06l3.72 3.72a.75.75 0 101.06-1.06L11.06 10l3.72-3.72a.75.75 0 00-1.06-1.06L10 8.94 6.28 5.22z" />
+                            </svg>
+                        </button>
+                    </div>
                 </div>
             </div>
         </Transition>
