b4bf9d033a21dbecad38ab2657bf0ec4cd380b46
Paul Bouzakis <888851+pbouzakis@users.noreply.github.com>
2023-11-20 19:20:24 -0500

    Add new control for confirm delete modals.

--- web-client/src/client/controls/Modal.ts
+++ web-client/src/client/controls/Modal.ts
@@ -2,6 +2,16 @@ import { ControlRegistry } from "../registery";
 import Toggle from "./Toggle";
 import query from "../query"
 
+type ConfirmDeleteCommand = {
+  title?: string,
+  message?: string,
+  confirmText?: string,
+  cancelText?: string,
+  deleteHref: string,
+  // actionConfirmed: () => void,
+  // actionCanceled?: () => void,
+};
+
 const Modal = {
   async attach(element: HTMLElement) {
     console.log("Modal::attach()", element);
@@ -17,11 +27,47 @@ const Modal = {
 
     await toggle.open();
     await togglePanel.open();
+  },
+
+  confirmDelete({
+    title,
+    message,
+    confirmText = "Delete",
+    cancelText = "cancel",
+    deleteHref,
+  }: ConfirmDeleteCommand) {
+    const tpl = query(document, "#tpl-confirm-delete-modal") as HTMLTemplateElement;
+    const modalElement = tpl.content.cloneNode(true) as HTMLElement;
+
+    const Elements = query.all(modalElement, {
+      title: "[data-confirm-delete-title]",
+      message: "[data-confirm-delete-message]",
+    });
+
+    if (title) {
+      Elements.title.textContent = title;
+    }
+    if (message) {
+      Elements.message.textContent = message;
+    }
+
+    const deleteActionElement = query(modalElement, "[data-confirm-action=\"delete\"]");
+    deleteActionElement.addEventListener("click", () => {
+      // TODO REMOVE hard dependency on htmx
+      (window as any).htmx.ajax("DELETE", deleteHref, { target: document.body, swap: "beforeend" });
+    }, { once: true });
+
+    const modalContentElement = query(document, "#modal-live-region [data-modal-content]");
+    modalContentElement.appendChild(modalElement);
   }
 };
 
 function init(registry: ControlRegistry) {
   registry.registerControl("modal", Modal);
+
+  registry.registerGlobalApi({
+    confirmDelete: Modal.confirmDelete,
+  });
 }
 
 export default {
--- web-client/src/server/modal.rs
+++ web-client/src/server/modal.rs
@@ -72,18 +72,22 @@ pub fn Modal(props: ModalProps) -> String {
     }
 }
 
+// TODO! Provide option to show close "X" link in modal
+/*
+    <div class="absolute right-0 top-0 hidden pr-4 pt-4 sm:block">
+        <button type="button" class="rounded-md bg-white text-gray-400 hover:text-gray-500 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2">
+            <span class="sr-only">Close</span>
+            <svg class="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" aria-hidden="true">
+                <path stroke-linecap="round" stroke-linejoin="round" d="M6 18L18 6M6 6l12 12" />
+            </svg>
+        </button>
+    </div>
+*/
+
 #[component]
 pub fn ConfirmDeleteModal() -> String {
     html! {
         <Modal>
-            <div class="absolute right-0 top-0 hidden pr-4 pt-4 sm:block">
-                <button type="button" class="rounded-md bg-white text-gray-400 hover:text-gray-500 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2">
-                    <span class="sr-only">Close</span>
-                    <svg class="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" aria-hidden="true">
-                        <path stroke-linecap="round" stroke-linejoin="round" d="M6 18L18 6M6 6l12 12" />
-                    </svg>
-                </button>
-            </div>
             <div class="sm:flex sm:items-start">
                 <div class="mx-auto flex h-12 w-12 flex-shrink-0 items-center justify-center rounded-full bg-red-100 sm:mx-0 sm:h-10 sm:w-10">
                     <svg class="h-6 w-6 text-red-600" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" aria-hidden="true">
@@ -91,15 +95,15 @@ pub fn ConfirmDeleteModal() -> String {
                     </svg>
                 </div>
                 <div class="mt-3 text-center sm:ml-4 sm:mt-0 sm:text-left">
-                    <h3 class="text-base font-semibold leading-6 text-gray-900" id="modal-title">Deactivate account</h3>
+                    <h3 class="text-base font-semibold leading-6 text-gray-900" id="modal-title" data-confirm-delete-title>Deactivate account</h3>
                     <div class="mt-2">
-                        <p class="text-sm text-gray-500">Are you sure you want to deactivate your account? All of your data will be permanently removed from our servers forever. This action cannot be undone.</p>
+                        <p class="text-sm text-gray-500" data-confirm-delete-message>Are you sure you want to delete this item?.</p>
                     </div>
                 </div>
             </div>
             <div class="mt-5 sm:mt-4 sm:flex sm:flex-row-reverse">
-                <button type="button" class="inline-flex w-full justify-center rounded-md bg-red-600 px-3 py-2 text-sm font-semibold text-white shadow-sm hover:bg-red-500 sm:ml-3 sm:w-auto">Deactivate</button>
-                <button type="button" class="mt-3 inline-flex w-full justify-center rounded-md bg-white px-3 py-2 text-sm font-semibold text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 hover:bg-gray-50 sm:mt-0 sm:w-auto">Cancel</button>
+                <button data-confirm-action="delete" type="button" class="inline-flex w-full justify-center rounded-md bg-red-600 px-3 py-2 text-sm font-semibold text-white shadow-sm hover:bg-red-500 sm:ml-3 sm:w-auto">Delete</button>
+                <button data-toggle-action="close" type="button" class="mt-3 inline-flex w-full justify-center rounded-md bg-white px-3 py-2 text-sm font-semibold text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 hover:bg-gray-50 sm:mt-0 sm:w-auto">Cancel</button>
             </div>
         </Modal>
     }
