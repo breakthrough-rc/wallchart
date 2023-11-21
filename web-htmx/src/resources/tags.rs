1223ed348c5f50d5bb5e27aa039ac6e68d662d41
Paul Bouzakis <888851+pbouzakis@users.noreply.github.com>
2023-11-20 19:55:21 -0500

    Minor cleanup.

--- web-htmx/src/resources/tags.rs
+++ web-htmx/src/resources/tags.rs
@@ -92,7 +92,7 @@ async fn get_tags(
                                                     <td class="whitespace-nowrap py-4 pl-4 pr-3 text-sm font-medium text-gray-900 sm:pl-6">{&tag.name}</td>
                                                     <td class="whitespace-nowrap px-3 py-4 text-sm text-gray-500">{&tag.icon}</td>
                                                     <td class="relative whitespace-nowrap py-4 pl-3 pr-4 text-right text-sm font-medium sm:pr-6">
-                                                        <div class="flex gap-8">
+                                                        <div class="inline-flex gap-4">
                                                             <a
                                                                 hx-get=format!("/worksites/{}/tags/{}/edit-form", &worksite_id, &tag.id)
                                                                 hx-target="body"
