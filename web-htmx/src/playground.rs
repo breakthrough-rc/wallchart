e2e05a081113f2eca1a4dbc8a9636e7ceecbc27c
Paul Bouzakis <888851+pbouzakis@users.noreply.github.com>
2023-11-21 13:23:16 -0500

    Move notification playground into its own module.

--- web-htmx/src/playground.rs
+++ web-htmx/src/playground.rs
@@ -1,35 +1,27 @@
 #![allow(unused_braces)]
-use super::playground::modal::{modal_routes, ModalPlayground};
 use crate::components::page::PageLayout;
-use axum::{
-    response::Html,
-    routing::{get, post},
-    Router,
-};
+use axum::{response::Html, routing::get, Router};
 use http::HeaderMap;
 use rscx::{component, html, props};
-use std::time::{SystemTime, UNIX_EPOCH};
+
+use modal::{modal_routes, ModalPlayground};
+use notifications::{notification_routes, NotificationsPlayground};
 use web_client::server::{
-    attrs::Attrs,
     button::{PrimaryButton, SecondaryButton},
     html_element::HtmlElement,
-    notification::{
-        NoticationCloseButton, NotificationCall, NotificationPresenter, NotificationTransition,
-    },
 };
 use web_macros::*;
 
 pub mod modal;
+pub mod notifications;
 
 pub fn routes() -> Router {
     Router::new()
         .route("/", get(get_playground))
         .route("/test-render", get(get_test_render))
         .route("/htmx", get(htmx_test))
-        .route("/ex-business-logic", post(ex_business_logic))
-        .route("/custom-notification", get(get_custom_notification))
-        .route("/custom-notification2", get(get_custom_notification2))
         .nest("/modals", modal_routes())
+        .nest("/notifications", notification_routes())
 }
 
 #[component]
@@ -104,61 +96,6 @@ fn HtmlElementPlayground() -> String {
                 <MessageButton message="This is a message from a button!".into()>
                     I am a MessageButton, click to see a message!
                 </MessageButton>
-            </div>
-        </section>
-    }
-}
-
-#[component]
-fn NotificationsPlayground() -> String {
-    html! {
-        <section class="py-8">
-            <h2 class="text-xl font-bold">Notifications Playground</h2>
-            <div class="flex flex-col gap-4">
-                <section>
-                    <p><em>Show a toast notification (client-side).</em></p>
-                    <div class="flex gap-2">
-                        <PrimaryButton
-                            onclick="YcControls.showSuccessNotification('Success feels so good!')"
-                        >
-                            Show Success
-                        </PrimaryButton>
-                        <PrimaryButton
-                            onclick="YcControls.showErrorNotification('This is an error notification.')"
-                        >
-                            Show Error
-                        </PrimaryButton>
-                        <PrimaryButton
-                            onclick="YcControls.showNotification('This just in', 'You are still not done!')"
-                        >
-                            Show Generic
-                        </PrimaryButton>
-                    </div>
-                </section>
-                <section>
-                    <p><em>Show a toast notification (server-side).</em></p>
-                    <div class="flex gap-2">
-                        <PrimaryButton
-                            hx_post="/playground/ex-business-logic"
-                        >
-                            Show Success
-                        </PrimaryButton>
-                        <PrimaryButton
-                            hx_get="/playground/custom-notification"
-                            hx_target="body"
-                            hx_swap="beforeend"
-                        >
-                            Show Custom
-                        </PrimaryButton>
-                        <PrimaryButton
-                            hx_get="/playground/custom-notification2"
-                            hx_target="body"
-                            hx_swap="beforeend"
-                        >
-                            Show Custom w/ Standard Components
-                        </PrimaryButton>
-                    </div>
-                </section>
             </div>
         </section>
     }
@@ -256,54 +193,6 @@ async fn get_playground() -> Html<String> {
         <PageLayout header="Component Playground">
             <PlaygroundPgContent />
         </PageLayout>
-    })
-}
-
-async fn get_custom_notification() -> Html<String> {
-    Html(html! {
-        <NotificationPresenter call=NotificationCall::Template>
-            <template>
-                <div class="bg-white p-10 border">
-                    <p>This is a bad notification!</p>
-                    <SecondaryButton attrs=Attrs::with("data-toggle-action", "close".into())>
-                        Close me
-                    </SecondaryButton>
-                </div>
-            </template>
-        </NotificationPresenter>
-    })
-}
-
-async fn get_custom_notification2() -> Html<String> {
-    Html(html! {
-        <NotificationPresenter call=NotificationCall::Template>
-            <template>
-                <NotificationTransition
-                    class="bg-white border w-full max-w-sm overflow-hidden shadow-lg"
-                >
-                    <div class="p-4">
-                        <div class="flex items-start">
-                            <p class="flex-1">Wow this looks a lil nicer.</p>
-                            <NoticationCloseButton />
-                        </div>
-                    </div>
-                </NotificationTransition>
-            </template>
-        </NotificationPresenter>
-    })
-}
-
-async fn ex_business_logic() -> Html<String> {
-    // Do some business logic here...
-    // Then we will tell the client to show a success notification.
-    let nanos = SystemTime::now()
-        .duration_since(UNIX_EPOCH)
-        .unwrap()
-        .subsec_nanos();
-
-    Html(html! {
-        {format!("Action complete. Result: {}!", nanos)}
-        <NotificationPresenter call=NotificationCall::Success(format!("Server side validated! Answer is {}", nanos)) />
     })
 }
 
