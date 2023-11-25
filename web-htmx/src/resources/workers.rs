16f87dbaf8ded526d38ae2f5340388a7a85c5d6c
Paul Bouzakis <888851+pbouzakis@users.noreply.github.com>
2023-11-25 12:27:54 -0500

    Clean up workers resource.

--- web-htmx/src/resources/workers.rs
+++ web-htmx/src/resources/workers.rs
@@ -126,40 +126,14 @@ async fn get_worker_details(
         <Flyout title=format!("Worker Detail: {}", &full_name)>
             <div class="w-full border-t border-gray-200 py-6">
                 <div class="flex flex-col gap-10">
-                    <section
-                        hx-get=routes::assigned_tags_form(&worksite_id, &worker_id)
-                        hx-trigger="revealed"
-                    >
-                        <svg class="animate-spin m-auto text-gray-100 h-10 w-10" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24">
-                            <circle class="opacity-25" cx="12" cy="12" r="10" stroke="currentColor" stroke-width="4"></circle>
-                            <path class="opacity-75" fill="currentColor" d="M4 12a8 8 0 018-8V0C5.373 0 0 5.373 0 12h4zm2 5.291A7.962 7.962 0 014 12H0c0 3.042 1.135 5.824 3 7.938l3-2.647z"></path>
-                        </svg>
-                    </section>
-                    <div class="relative">
-                        <div class="absolute inset-0 flex items-center" aria-hidden="true">
-                            <div class="w-full border-t border-gray-300"></div>
-                        </div>
-                        <div class="relative flex justify-center">
-                            <span class="bg-white px-2 text-sm text-gray-500">Continue</span>
-                        </div>
-                    </div>
-                    <section
-                        hx-get=routes::assessments(&worksite_id, &worker_id)
-                        hx-trigger="revealed"
-                    >
-                        <svg class="animate-spin m-auto text-gray-100 h-10 w-10" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24">
-                            <circle class="opacity-25" cx="12" cy="12" r="10" stroke="currentColor" stroke-width="4"></circle>
-                            <path class="opacity-75" fill="currentColor" d="M4 12a8 8 0 018-8V0C5.373 0 0 5.373 0 12h4zm2 5.291A7.962 7.962 0 014 12H0c0 3.042 1.135 5.824 3 7.938l3-2.647z"></path>
-                        </svg>
-                    </section>
-                    <div class="relative">
-                        <div class="absolute inset-0 flex items-center" aria-hidden="true">
-                            <div class="w-full border-t border-gray-300"></div>
-                        </div>
-                        <div class="relative flex justify-center">
-                            <span class="bg-white px-2 text-sm text-gray-500">Continue</span>
-                        </div>
-                    </div>
+                    <LoadingWorkerSection
+                        route=routes::assigned_tags_form(&worksite_id, &worker_id)
+                    />
+                    <SectionDivider />
+                    <LoadingWorkerSection
+                        route=routes::assessments(&worksite_id, &worker_id)
+                    />
+                    <SectionDivider />
                     <WorkerProfileSection
                         worker_id=worker_id.clone()
                         worksite_id=worksite_id.clone()
@@ -398,5 +372,39 @@ fn WorkerProfileSection(props: WorkerProfileSectionProps) -> String {
                 </Card>
             </form>
         </section>
+    }
+}
+
+#[props]
+struct LoadingWorkerSectionProps {
+    route: String,
+}
+
+#[component]
+fn LoadingWorkerSection(props: LoadingWorkerSectionProps) -> String {
+    html! {
+        <section
+            hx-get=props.route
+            hx-trigger="revealed"
+        >
+            <svg class="animate-spin m-auto text-gray-100 h-10 w-10" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24">
+                <circle class="opacity-25" cx="12" cy="12" r="10" stroke="currentColor" stroke-width="4"></circle>
+                <path class="opacity-75" fill="currentColor" d="M4 12a8 8 0 018-8V0C5.373 0 0 5.373 0 12h4zm2 5.291A7.962 7.962 0 014 12H0c0 3.042 1.135 5.824 3 7.938l3-2.647z"></path>
+            </svg>
+        </section>
+    }
+}
+
+#[component]
+pub fn SectionDivider() -> String {
+    html! {
+        <div class="relative">
+            <div class="absolute inset-0 flex items-center" aria-hidden="true">
+                <div class="w-full border-t border-gray-300"></div>
+            </div>
+            <div class="relative flex justify-center">
+                <span class="bg-white px-2 text-sm text-gray-500">Continue</span>
+            </div>
+        </div>
     }
 }
