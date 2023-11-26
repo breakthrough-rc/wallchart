90a87dd27101c1b197b173ccdd0b000229cd1ac6
tavish <tapegram@gmail.com>
2023-11-26 12:13:56 -0600

    WIP: deserializing CSV input

--- web-htmx/src/resources/csv_upload.rs
+++ web-htmx/src/resources/csv_upload.rs
@@ -5,6 +5,7 @@ use axum::{
     Router,
 };
 use rscx::html;
+use serde::Deserialize;
 use std::str::from_utf8;
 
 use web_client::server::{card::Card, form::Button};
@@ -41,20 +42,72 @@ async fn get_csv_upload(State(_state): State<WebHtmxState>) -> impl IntoResponse
     })
 }
 
+/**
+* This is an example CSV row. We use this to construct all of the
+* worksites/locations/shifts/workers as needed.
+*
+* There is an enourmous amount of room for improvement but this is a quick and
+* dirty onboarding tool. We should improve this process as we encounter real
+* life usecases.
+*
+* At the moment, this code assumes that worksites, locations, shifts etc. are names that
+* are relatively unique (to their context) so we can create/reuse these resources
+* as needed
+*/
+#[derive(Debug, Deserialize)]
+struct WorkerRecord {
+    worksite: String,
+    location: String,
+    shift1: String,
+    shift2: String,
+    shift3: String,
+    name: String,
+    email: String,
+    phone: String,
+}
 async fn post_csv_upload(
     State(_state): State<WebHtmxState>,
     mut multipart: Multipart,
 ) -> impl IntoResponse {
-    let mut context: Vec<String> = vec![];
+    let mut content: Vec<String> = vec![];
+    /*
+     * First we process the multipart input to get a vec of the file contents
+     *
+     * Multipart processing is based on the breaking the file up into arb chunks,
+     * not line by line. So we have to reassemble the file contents from the chunks.
+     */
     while let Some(field) = multipart.next_field().await.unwrap() {
         let bytes = field.bytes().await.unwrap();
         let data = from_utf8(&bytes).unwrap();
-        context.push(data.to_string());
+        content.push(data.to_string());
     }
+
+    println!("context: {:#?}", content);
+
+    /*
+     * Now we have the file contents, we can process it as a CSV
+     *
+     * We combine the "chunks" into a single string and then deserialize row by row
+     */
+    let content: String = content.join("");
+    let mut rdr = csv::Reader::from_reader(content.as_bytes());
+    let mut records: Vec<WorkerRecord> = vec![];
+    for result in rdr.deserialize() {
+        let record: WorkerRecord = result.unwrap();
+        records.push(record);
+    }
+
+    println!("records: {:#?}", records);
 
     Html(html! {
         <PageLayout header="Upload a CSV">
-            <p>You uploaded: {context.join(", ")}</p>
+            <p><em>Bulk upload data into your worksites.</em></p>
+            <PageContent>
+                <Card padded=true>
+                    <p>Here is the data we received:</p>
+                    <pre>{format!("{:#?}", records)}</pre>
+                </Card>
+            </PageContent>
         </PageLayout>
     })
 }
