9ea0804c7c4451c3c3b2e7cfba0702d52e89fd35
Paul Bouzakis <888851+pbouzakis@users.noreply.github.com>
2023-11-29 20:47:28 -0500

    Support title inside PageContent.

--- web-htmx/src/components/page_content.rs
+++ web-htmx/src/components/page_content.rs
@@ -2,12 +2,24 @@ use rscx::{component, html, props};
 
 #[props]
 pub struct PageContentProps {
+    #[builder(setter(into), default = "".into())]
+    title: String,
+
     children: String,
 }
 
 #[component]
 pub fn PageContent(props: PageContentProps) -> String {
+    let title = if props.title.is_empty() {
+        "".into()
+    } else {
+        html! {
+            <p><em>{props.title}</em></p>
+        }
+    };
+
     html! {
+        {title}
         <div class="flex flex-col">
             <div class="mt-8 flow-root">
                 <div class="-mx-4 -my-2 overflow-x-auto sm:-mx-6 lg:-mx-8">
