0386d7254552d6f16f3437a0bae2d0ca22aef6d2
Paul Bouzakis <888851+pbouzakis@users.noreply.github.com>
2023-12-06 19:45:39 -0500

    Render a dummy error state when add worker form post is invalid.

--- web-htmx/src/resources/workers.rs
+++ web-htmx/src/resources/workers.rs
@@ -203,7 +203,31 @@ async fn post_worker(
     Form(form): Form<WorkerProfileFormData>,
 ) -> Response {
     if let Err(e) = form.validate() {
-        return (StatusCode::BAD_REQUEST, format!("Invalid form. {}", e)).into_response();
+        return (
+            StatusCode::BAD_REQUEST,
+            Html(html! {
+                <div class="mt-6 rounded-md bg-red-50 p-4">
+                    <div class="flex">
+                        <div class="flex-shrink-0">
+                            <svg class="h-5 w-5 text-red-400" viewBox="0 0 20 20" fill="currentColor" aria-hidden="true">
+                                <path fill-rule="evenodd" d="M10 18a8 8 0 100-16 8 8 0 000 16zM8.28 7.22a.75.75 0 00-1.06 1.06L8.94 10l-1.72 1.72a.75.75 0 101.06 1.06L10 11.06l1.72 1.72a.75.75 0 101.06-1.06L11.06 10l1.72-1.72a.75.75 0 00-1.06-1.06L10 8.94 8.28 7.22z" clip-rule="evenodd" />
+                            </svg>
+                        </div>
+                        <div class="ml-3">
+                            <h3 class="text-sm font-medium text-red-800">There were 2 errors with your submission</h3>
+                            <div class="mt-2 text-sm text-red-700">
+                            <ul role="list" class="list-disc space-y-1 pl-5">
+                                <li>Your password must be at least 8 characters</li>
+                                <li>Your password must include at least one pro wrestling finishing move</li>
+                            </ul>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <WorkerForm action=workers_new(&wallchart_id) />
+            }),
+        )
+            .into_response();
     }
 
     worksite_service
@@ -335,7 +359,12 @@ struct WorkerFormProps {
 #[component]
 fn WorkerForm(props: WorkerFormProps) -> String {
     html! {
-        <form hx-post=props.action>
+        <form
+            hx-ext="response-targets"
+            id="form-worker"
+            hx-post=props.action
+            hx-target-4xx="this"
+        >
             <div class="pb-12">
                 <WorkerProfileFieldset />
             </div>
